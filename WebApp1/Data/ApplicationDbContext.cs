2b8a91dbae7c92a3d2ca065f3df0ef8d848c3a01
dania <daniahilal@gmail.com>
2026-01-24 16:11:38 +0200

    connection to DB

--- WebApp1/Data/ApplicationDbContext.cs
+++ WebApp1/Data/ApplicationDbContext.cs
@@ -6,8 +6,13 @@ namespace KaShop.Data
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
+
             base.OnConfiguring(optionsBuilder);
-            optionsBuilder.UseSqlServer("Server=.;database=kashop;Trusted_Connection=True;TrustServerCertificate=True");
+            //db for production
+            optionsBuilder.UseSqlServer("Server=db38977.databaseasp.net; Database=db38977; User Id=db38977; Password=n%8WPi?79+Xt; Encrypt=False; MultipleActiveResultSets=True;");
+           
+            //db for development
+            //optionsBuilder.UseSqlServer("Server=.;database=kashop;Trusted_Connection=True;TrustServerCertificate=True");
         }
     }
 }
