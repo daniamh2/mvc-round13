24a2d02339b453321f59c790307d8a55ba5db665
dania <daniahilal@gmail.com>
2026-01-24 15:50:06 +0200

    connectiom=n to DB

---
+++ WebApp1/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace KaShop.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=.;database=kashop;Trusted_Connection=True;TrustServerCertificate=True");
+        }
+    }
+}
